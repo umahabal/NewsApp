a9865147f548ab6d5117f417ba3ee0d3e72054c0
Unmesh <mahabu2@medtronic.com>
2022-06-13 20:14:42 +0530

    Changes related to Firebase crashlytics
    Firebase Crashlytics Integration

--- NewsAPI/NewsAPIApp.swift
+++ NewsAPI/NewsAPIApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+
 
 @main
 struct NewsAPIApp: App {
+    
+    init() {
+        // Use the Firebase library to configure APIs.
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
 //            ContentView()
+            
             NewListView()
         }
     }
